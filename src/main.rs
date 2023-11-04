17dcd205d8e3591f4dd21c2dbcb35cfaba10c099
mrmtsu <81202muramatsu@gmail.com>
2023-11-04 16:32:56 +0900

    add actix_web

--- src/main.rs
+++ src/main.rs
@@ -1,118 +1,17 @@
-use std::{
-    fs::File,
-    io::BufReader,
-    io::{stdin, BufRead},
-    path::PathBuf,
-};
+use actix_web::{get, App, HttpResponse, HttpServer};
 
-use clap::Parser;
+#[get("/")]
+async fn index() -> Result<HttpResponse, actix_web::Error> {
+    let response_body = "Hello world!";
 
-#[derive(Parser, Debug)]
-#[clap(
-    name = "My RPN program",
-    version = "1.0.0",
-    author = "Your name",
-    about = "Super awesome sample RPN calcuator"
-)]
-struct Opts {
-    //  Set the level of verbosity
-    #[clap(short, long)]
-    verbose: bool,
-
-    // Formulas written in RPN
-    #[clap(name = "FILE")]
-    formula_file: Option<PathBuf>,
+    Ok(HttpResponse::Ok().body(response_body))
 }
 
-fn main() {
-    let opts = Opts::parse();
-    if let Some(path) = opts.formula_file {
-        let f = File::open(path).unwrap();
-        let reader = BufReader::new(f);
-        run(reader, opts.verbose);
-    } else {
-        let stdin = stdin();
-        let reader = stdin.lock();
-        run(reader, opts.verbose);
-        println!("No file is specified");
-    }
-}
-
-fn run<R: BufRead>(reader: R, verbose: bool) {
-    let calc = RpnCalculator::new(verbose);
-    for line in reader.lines() {
-        let line = line.unwrap();
-        let answer = calc.eval(&line);
-        println!("{}", answer);
-    }
-}
-
-struct RpnCalculator(bool);
-impl RpnCalculator {
-    pub fn new(verbose: bool) -> Self {
-        Self(verbose)
-    }
-
-    pub fn eval(&self, formula: &str) -> i32 {
-        let mut tokens = formula.split_whitespace().rev().collect::<Vec<_>>();
-        self.eval_inner(&mut tokens)
-    }
-
-    fn eval_inner(&self, tokens: &mut Vec<&str>) -> i32 {
-        let mut stack = Vec::new();
-
-        while let Some(token) = tokens.pop() {
-            if let Ok(x) = token.parse::<i32>() {
-                stack.push(x);
-            } else {
-                let y = stack.pop().expect("invalid syntax");
-                let x = stack.pop().expect("invalid syntax");
-                let res = match token {
-                    "+" => x + y,
-                    "-" => x - y,
-                    "*" => x * y,
-                    "/" => x / y,
-                    "%" => x % y,
-                    _ => panic!("invalid token"),
-                };
-                stack.push(res);
-            }
-
-            if self.0 {
-                println!("{:?} {:?}", tokens, stack);
-            }
-        }
-
-        if stack.len() == 1 {
-            stack[0]
-        } else {
-            panic!("invalid syntax")
-        }
-    }
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn test_ok() {
-        let calc = RpnCalculator::new(false);
-        assert_eq!(calc.eval("5"), 5);
-        assert_eq!(calc.eval("50"), 50);
-        assert_eq!(calc.eval("-50"), -50);
-
-        assert_eq!(calc.eval("2 3 +"), 5);
-        assert_eq!(calc.eval("2 3 *"), 6);
-        assert_eq!(calc.eval("2 3 -"), -1);
-        assert_eq!(calc.eval("2 3 /"), 0);
-        assert_eq!(calc.eval("2 3 %"), 2);
-    }
-
-    #[test]
-    #[should_panic]
-    fn test_ng() {
-        let calc = RpnCalculator::new(false);
-        calc.eval("1 1 ~");
-    }
+#[actix_rt::main]
+async fn main() -> Result<(), actix_web::Error> {
+    HttpServer::new(move || App::new().service(index))
+        .bind("0.0.0.0:8080")?
+        .run()
+        .await?;
+    Ok(())
 }
